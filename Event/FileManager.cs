443a3695b970b9be9ac88573cdea444905824782
MSR <mazitov@ukr.net>
2017-12-06 19:11:32 +0200

    Add project files.

---
+++ Event/FileManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    public class FileManager
+    {
+        public delegate void FileFoundHandler();
+        public event FileFoundHandler FileFound;
+
+        public void Search(bool isFound)
+        {
+            if (FileFound != null)
+            {
+                OnFileFound();
+            }
+        }
+
+        protected virtual void OnFileFound()   // этот метод события можно будет переопределить в наследнике
+        {
+            if (FileFound != null)
+            {
+                FileFound();
+            }
+        }
+    }
+}
---
+++ Event/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    class Program
+    {
+        static void Test()
+        {
+            Console.WriteLine("Test");
+        }
+
+
+        static void Main(string[] args)
+        {
+            FileManager manager = new FileManager();
+            manager.FileFound += Test;
+            manager.Search(true);
+            manager.FileFound -= Test;
+            manager.FileFound -= Test;
+            //---------------------------------------
+            //manager.FileFound += () => Console.WriteLine("FOUND");
+            ////---------------------------------------
+            //manager.FileFound += () =>
+            //{
+            //    Console.WriteLine("FOUND2");
+            //};
+            ////---------------------------------------
+            manager.Search(true);
+
+            Console.ReadKey();
+        }
+    }
+}
